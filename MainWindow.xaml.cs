bc789c8e5a5c12bf10d7ba48b4dd21a8e03dafe4
Parker Hughes <pshughes1@liberty.edu>
2026-02-05 17:29:56 -0500

    feat: real passwords added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,11 +28,17 @@ namespace Paw_somePasswordProtector
 
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
-            if (tbxInput.Text == "SecretPassword")
+            if (tbxInput.Text == "Hoppo")
             {
                 // disable grid1
                 Grid1.IsEnabled = false;
                 Grid1.Visibility = Visibility.Hidden;
+
+                tb_Pass1.Text = "ILoveMyDog";
+                tb_Pass2.Text = "TL_9anRi#$1p";
+                tb_Pass3.Text = "Nl])1l*7jwkl";
+                tb_Pass4.Text = "NALLkwdow(181";
+                tb_Pass5.Text = "LAlf;l]1+001o";
 
                 // enable grid2
                 Grid2.IsEnabled = true;
